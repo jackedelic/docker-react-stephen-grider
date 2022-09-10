aa0371c324a778533ce9b2c60f05d3a413d894c7
jack.wong <jack.wong@bytedance.com>
2022-09-10 20:08:28 +0800

    edit text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React :D
         </a>
       </header>
     </div>
